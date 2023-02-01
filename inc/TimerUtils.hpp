a342ec2cb9a1f32046e896eafac5b5fa756a8c47
Sriram636 <sriram.saiganesh@gmail.com>
2023-01-31 22:18:16 -0500

    implemented SortTimer with high_resolution_clock

--- inc/TimerUtils.hpp
+++ inc/TimerUtils.hpp
@@ -2,10 +2,23 @@
 #include <array>
 #include <typeinfo>
 
+#include <chrono>
+
+typedef std::chrono::high_resolution_clock Clock;
+
 using namespace std;
 
 namespace TimerUtils{
 
-	
+// function to time any ArrayUtil::Sort algorithm
+template <class T, size_t len, class F> 
+void SortTimer(F sorter, array<T, len>& ar){
+
+    auto begin = Clock::now();
+	// call function
+	sorter(ar);
+	auto elapsed = (Clock::now()- begin);
+    std::cout<<"Steady Time measured: "<< chrono::duration_cast<chrono::microseconds>(elapsed).count()<<" microseconds.\n";
+}
 
 }		// end namespace
