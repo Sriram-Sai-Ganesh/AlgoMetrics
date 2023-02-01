1533da5082c39cf2abcb45b07f5e7d833b1a0db9
Sriram636 <sriram.saiganesh@gmail.com>
2023-01-31 23:05:12 -0500

    added SearchTimer for search functions. TODO: varargs timer function to handle any type

--- inc/TimerUtils.hpp
+++ inc/TimerUtils.hpp
@@ -17,8 +17,22 @@ void SortTimer(F sorter, array<T, len>& ar){
     auto begin = Clock::now();
 	// call function
 	sorter(ar);
+	// double sum = 0;
+	// for(int i=0;i<1000*1000;i++) sum += 1;
 	auto elapsed = (Clock::now()- begin);
-    std::cout<<"Steady Time measured: "<< chrono::duration_cast<chrono::microseconds>(elapsed).count()<<" microseconds.\n";
+	double micro = chrono::duration_cast<chrono::microseconds>(elapsed).count();
+    std::cout<<"wall time measured: "<< micro<<" microseconds.\n";
+}
+
+// function to time any ArrayUtil::Search algorithm
+template <class T, size_t len, class F> 
+void SearchTimer(F searcher, array<T, len>& ar, T target){
+    auto begin = Clock::now();
+	// call function
+	searcher(ar);
+	auto elapsed = (Clock::now()- begin);
+	double micro = chrono::duration_cast<chrono::microseconds>(elapsed).count();
+    std::cout<<"wall time measured: "<< micro<<" microseconds.\n";
 }
 
 }		// end namespace
