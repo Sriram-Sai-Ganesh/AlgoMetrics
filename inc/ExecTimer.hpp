93195757c296d48bb71618595177d7ad1ef00318
Sriram636 <sriram.saiganesh@gmail.com>
2023-02-09 13:46:04 -0500

    replaced TimerUtils with generic ExecTimer decorator

---
+++ inc/ExecTimer.hpp
@@ -0,0 +1,51 @@
+#include <cstdlib>
+#include <array>
+#include <functional>
+#include <typeinfo>
+#include <chrono>
+
+#pragma once
+
+using namespace std;
+
+typedef chrono::high_resolution_clock Clock;
+
+// forward declaration
+template<class R, class... args> class ExecTimer;
+
+// struct for generic FunctionTimers
+template<class R, class... args> 
+struct ExecTimer<R(args ...)>{
+	private:
+		// member variables
+		function<R(args...)> myFunc;
+		float elapsed;
+	public:
+
+		// constructor
+		ExecTimer(function<R(args...)> func){
+			elapsed=-1;
+			myFunc=func;
+		}
+	
+		// overload () operator
+		float operator()(args ... arguments){
+			auto begin = Clock::now();
+			// ignore return value
+			// R result=myFunc(arguments...);	
+			myFunc(arguments...);	
+			elapsed=chrono::duration_cast<chrono::microseconds>(Clock::now()-begin).count();
+			return elapsed;
+		}
+
+		// return timer result in microseconds
+		// -1 if function has not been run.
+		float Count(){
+			return elapsed;
+		}
+};
+
+template<class R, class... args> 
+ExecTimer<R(args ...)> CreateTimer(R (*func)(args ...)) {
+    return ExecTimer<R(args...)>(function<R(args...)>(func));    
+}
