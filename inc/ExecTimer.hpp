9e7483b8560c6687fbd8358afa4b9839edb47890
Sriram636 <sriram.saiganesh@gmail.com>
2023-02-09 17:14:11 -0500

    added detailed comments

--- inc/ExecTimer.hpp
+++ inc/ExecTimer.hpp
@@ -13,41 +13,49 @@ typedef chrono::high_resolution_clock Clock;
 // forward declaration
 template<class R, class... args> class ExecTimer;
 
-
-// struct for generic FunctionTimers
+/// @brief struct for generic FunctionTimers
+/// @tparam R function return type
+/// @tparam ...args types of params
 template<class R, class... args> 
 struct ExecTimer<R(args ...)>{
 	private:
 		// member variables
 		function<R(args...)> myFunc;
-		int elapsed;
+		float elapsed;
 	public:
 
-		// constructor
+		/// @brief constructor for ExecTimer object
+		/// @param func function using which ExecTimer object will be created.
 		ExecTimer(function<R(args...)> func){
 			elapsed=-1;
 			myFunc=func;
 		}
 	
-		// overload () operator
-		float operator()(args ... arguments){
+		/// @brief overloaded operator: parantheses
+		/// @param ...params arguments for the function to be called. Listed in order. 
+		/// @return float execution duration in microseconds
+		float operator()(args ... params){
 			auto begin = Clock::now();
-			// ignore return value
+			// not using return value
 			// R result=myFunc(arguments...);	
-			myFunc(arguments...);	
+			myFunc(params...);	
 			elapsed=chrono::duration_cast<chrono::microseconds>(Clock::now()-begin).count();
 			return elapsed;
 		}
 
-		// return timer result in microseconds
-		// -1 if function has not been run.
+		/// @brief returns value of execution time variable
+		/// @return time taken to execute function. -1 if uninitialized.
 		float Count(){
 			return elapsed;
 		}
 };
 
-// wrapper function to constructor
+/// @brief wrapper to ExecTimer constructor
+/// @tparam R return type of @param func 
+/// @tparam ...args parameters of @param func. Listed in order.
+/// @param func function using which an ExecTimer will be created
+/// @return time taken to execute @param func with @param ...args
 template<class R, class... args> 
 ExecTimer<R(args ...)> CreateTimer(R (*func)(args ...)) {
-    return ExecTimer<R(args...)>(function<R(args...)>(func));
+    return ExecTimer<R(args...)>(function<R(args...)>(func));    
 }
