1e163c0712612b45a593deffe9c0d82d466e23fa
ram <35802333+Sriram-Sai-Ganesh@users.noreply.github.com>
2023-02-15 10:00:37 -0500

    edited to time sorting functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,54 @@
 #include "../inc/ArrayUtils.hpp"
 #include "../inc/HelperUtils.hpp"
 #include "../inc/ExecTimer.hpp"
-#include "../inc/LinkedList.hpp"
+#include "../inc/SinglyLinkedList.hpp"
+#include "../inc/DoublyLinkedList.hpp"
 
 typedef int myType;					// type of variable
-const size_t len = 100;				// array length
+const size_t len = 10000;				// array length
+const size_t numIterations = 120;				// number of timers to run
 const unsigned char mu=230;		// 230 : µ
 
 using namespace std;
+
+void trySinglyLinkedList(){
+	cout<<"LinkedLists: \n";
+	SinglyLinkedList<myType> link;
+	link.Add(1);
+	link.Add(2);
+	link.Add(3);
+	link.Add(4);
+	link.Add(5);
+	HelperUtils::PrintSinglyLinkedList<myType>(link);	
+	
+	cout<<"\n3rd index node is "<<link(3)<<endl;
+	HelperUtils::PrintSinglyLinkedList<myType>(link);	
+	link.RemoveNode(4);
+	link.RemoveValue(2);
+	cout<<"\n2nd index node is "<<link(2)<<endl;
+	HelperUtils::PrintSinglyLinkedList<myType>(link);	
+	endl(cout);
+}
+
+
+void tryDoublyLinkedList(){
+	cout<<"LinkedLists: \n";
+	DoublyLinkedList<myType> link;
+	link.Add(1);
+	link.Add(2);
+	link.Add(3);
+	link.Add(4);
+	link.Add(5);
+	HelperUtils::PrintDoublyLinkedList<myType>(link);	
+	
+	cout<<"\n3rd index node is "<<link(3)<<endl;
+	HelperUtils::PrintDoublyLinkedList<myType>(link);	
+	link.RemoveNode(4);
+	link.RemoveValue(2);
+	cout<<"\n2nd index node is "<<link(2)<<endl;
+	HelperUtils::PrintDoublyLinkedList<myType>(link);	
+	endl(cout);
+}
 
 int main()
 {
@@ -19,43 +60,44 @@ int main()
 	myType min=0, max=len*3;
 	// initialize array
 	auto randArray = HelperUtils::GetUniformRandomArray<myType, len>(min, max-1);
+	// create an ExecTimer object
+	auto funcTimer = CreateTimer(ArrayUtils::QuickSort<myType, len>);
 	
-	auto funcTimer = CreateTimer(ArrayUtils::TailRecursiveQuickSort<myType, len>);
-	out<<"\nInitial:\n";
-	// HelperUtils::PrintArray<myType, len>(randArray, out);
+	// print initial array.
+	// (comment out if array is large)
+	// HelperUtils::PrintArray<myType, len>(randArray, out); 
+	// endl(out, 2);
+
+	// store times in an array
+    array<int, numIterations> timeArray;
+	// run the timer 'numIterations' times
+	out<<"\nRunning timers...\n";
+	auto unsorted = randArray;
+	for(int i=0;i< (int)numIterations;i++){
+		unsorted=randArray;
+		int result = funcTimer(unsorted);
+		// out<<result<<" "<<mu<<"s\n";
+		timeArray[i]=result;
+	}
+	// sort the array of times
+	ArrayUtils::TailRecursiveQuickSort<myType, numIterations>(timeArray);
+	// print final array
+	// out<<"\nFinal:\n";	// comment out if array is large
+	// HelperUtils::PrintArray<myType, len>(unsorted);
+	out<<"list of times is \n";
+	float sum=0;
+	for(int i=10;i<(int)numIterations-10;i++){
+		cout<<timeArray[i]<<" "<<mu<<" seconds\n";
+		sum+=timeArray[i];
+    }
+	sum/=(numIterations-20.0);
+
+	out<<"\nAverage of median "<<(numIterations-20)<<" runs is "<<sum<<" "<<mu<<" seconds.";
+
+	
 	endl(out, 2);
 
-	auto unsorted = randArray;
-	int iterations = 100;
-	for(int i=0;i<iterations;i++){
-		unsorted=randArray;
-		cout<<funcTimer(unsorted)<<" "<<mu<<"s\n";
-	}
-	
-	out<<"\nFinal:\n";
-	HelperUtils::PrintArray<myType, len>(unsorted);
-	endl(out, 3);
-
-	tryLinkedLists();
+	// trySinglyLinkedList();
+	// tryDoublyLinkedList();
 	return 0;
-}
-
-void tryLinkedLists(){
-	
-	cout<<"LinkedLists: \n";
-	SinglyLinkedList<myType> link;
-	link.Add(1);
-	link.Add(2);
-	link.Add(3);
-	link.Add(4);
-	link.Add(5);
-	HelperUtils::PrintLinkedList<myType>(link);	
-	
-	cout<<"\n3rd index node is "<<link(3)<<endl;
-	HelperUtils::PrintLinkedList<myType>(link);	
-	link.RemoveNode(4);
-	link.RemoveValue(2);
-	cout<<"\n2nd index node is "<<link(2)<<endl;
-	HelperUtils::PrintLinkedList<myType>(link);	
-	endl(cout);
 }
