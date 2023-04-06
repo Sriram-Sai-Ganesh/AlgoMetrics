dcdb954bc2071559ab94d10f2be18a9ad85db223
Sriram636 <sriram.saiganesh@gmail.com>
2023-04-06 02:23:45 -0400

    interface for trees

---
+++ class/ITree.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include "IGraph.hpp"
+using namespace std;
+
+template<class T>
+class ITree : IGraph<T>{
+	virtual size_t Height() = 0;
+	virtual size_t Size() = 0;
+	virtual void Insert(T arg) = 0;
+	virtual bool Contains(T arg) = 0;
+};
